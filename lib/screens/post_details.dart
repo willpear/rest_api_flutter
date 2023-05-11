808ec5e6f5aec44859dbe65cff6dc01edc447b95
willpear <williansdepaula@gmail.com>
2023-05-11 14:48:47 -0400

    desenvolvido a mostra de detalhes

--- lib/screens/post_details.dart
+++ lib/screens/post_details.dart
@@ -1,10 +1,70 @@
 import 'package:flutter/material.dart';
+import 'package:rest_api/HTTPHelper.dart';
+import 'package:rest_api/screens/edit_post.dart';
 
 class PostDetails extends StatelessWidget {
-  const PostDetails({Key? key}) : super(key: key);
+  PostDetails(this.itemId, {Key? key}) : super(key: key) {
+    // Pegar o id no construtor
+    _futurePost = HTTPHelper().getItemById(itemId);
+  }
+
+  String itemId;
+  late Future<Map> _futurePost;
+  late Map post;
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Details"),
+        actions: [
+          IconButton(
+              onPressed: () {
+                Navigator.of(context).push(
+                    // Faz o redirecionamento para a página de edição, passando o Map de dados
+                    MaterialPageRoute(builder: (_) => EditPost(post)));
+              },
+              icon: Icon(Icons.edit)),
+          IconButton(
+              onPressed: () async {
+                // Faz o redirecionamento para a página de exclusão, passando id como parâmetro
+                bool deleted = await HTTPHelper().deleteItem(itemId);
+
+                if (deleted) {
+                  ScaffoldMessenger.of(context)
+                      .showSnackBar(SnackBar(content: Text('Post excluído')));
+                } else {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(content: Text('Falha na exclusão do post')));
+                }
+              },
+              icon: Icon(Icons.delete)),
+        ],
+      ),
+      body: FutureBuilder<Map>(
+        future: _futurePost,
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return Center(child: Text('Ocorreu algum erro ${snapshot.error}'));
+          }
+
+          if (snapshot.hasData) {
+            post = snapshot.data!;
+
+            return Column(
+              children: [
+                Text(
+                  '${post['title']}',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                Text('${post['body']}'),
+              ],
+            );
+          }
+
+          return Center(child: CircularProgressIndicator());
+        },
+      ),
+    );
   }
 }
--- lib/screens/posts_list.dart
+++ lib/screens/posts_list.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:rest_api/HTTPHelper.dart';
+import 'post_details.dart';
 
 class PostsList extends StatelessWidget {
   PostsList({Key? key}) : super(key: key);
@@ -11,38 +12,42 @@ class PostsList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-      title: Text("Posts"),
-    ),
-    body: FutureBuilder<List<Map>>(
-      future: _futurePosts,
-      builder: (BuildContext context, AsyncSnapshot snapshot) {
-        // Verificar os erros
-        if (snapshot.hasError) {
-          return Center(child: Text("Ocorreu algum erro ${snapshot.error}"));
-        }
-
-        // Verificar se algum dado chegou
-        if (snapshot.hasData) {
-          List<Map> posts = snapshot.data;
-
-          return ListView.builder(
-            itemCount: posts.length,
-            itemBuilder: (context, index) {
-              Map thisItem = posts[index];
-              // Preenche a lista com os dados vindo da API
-              return ListTile(
-                title: Text('${thisItem['title']}'),
-                subtitle: Text('${thisItem['body']}'),
-                // Propriedade para tocar e exibir os detalhes dos dados
-              );
+          title: Text("Posts"),
+        ),
+        body: FutureBuilder<List<Map>>(
+          future: _futurePosts,
+          builder: (BuildContext context, AsyncSnapshot snapshot) {
+            // Verificar os erros
+            if (snapshot.hasError) {
+              return Center(
+                  child: Text("Ocorreu algum erro ${snapshot.error}"));
             }
-          );
-        }
 
-        // Apresentar o loader
-        return Center(child: CircularProgressIndicator());
-      },
-    )
-    );
+            // Verificar se algum dado chegou
+            if (snapshot.hasData) {
+              List<Map> posts = snapshot.data;
+
+              return ListView.builder(
+                  itemCount: posts.length,
+                  itemBuilder: (context, index) {
+                    Map thisItem = posts[index];
+                    // Preenche a lista com os dados vindo da API
+                    return ListTile(
+                      title: Text('${thisItem['title']}'),
+                      subtitle: Text('${thisItem['body']}'),
+                      // Propriedade para tocar e exibir os detalhes dos dados
+                      onTap: () {
+                        Navigator.of(context).push(MaterialPageRoute(
+                            builder: (context) =>
+                                PostDetails(thisItem['id'].toString())));
+                      },
+                    );
+                  });
+            }
+
+            // Apresentar o loader
+            return Center(child: CircularProgressIndicator());
+          },
+        ));
   }
 }
